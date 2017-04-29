be13c515618854228d0fade00bd614d47df099cc
Daniel Whatley <danieljwhatley@gmail.com>
2017-04-29 13:13:45 -0400

    Integrated yargs and added listing option

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
 var fs = require('fs-extra');
 var https = require('https');
 
+
 var settings = require('./settings');
 
-var doFetch = function () {
-    let state = process.argv[3];
-    if (!state || !settings.states[state.toLowerCase()]) {
-        console.error('Error: Invalid argument for \'state/territory\': ' + state);
-        process.exit(1);
-    }
-    state = state.toLowerCase();
 
-    let house = process.argv[4];
-    if (!house || !settings.houses[house.toLowerCase()]) {
-        console.error('Error: Invalid argument for \'house\': ' + house);
-        process.exit(1);
-    }
-    house = house.toLowerCase();
-
+var doFetch = function (state, house) {
     let TIGER_URL = settings.tigerUrl;
-
-
-    let url = 'https://www2.census.gov/geo/tiger/TIGER2016/' + settings.houses[house] + '/tl_2016_' + settings.states[state] + '_' + settings.houses[house].toLowerCase() + '.zip';
-
+    let filename = 'tl_2016_' + settings.states[state] + '_' + settings.houses[house].toLowerCase();
+    let url = 'https://www2.census.gov/geo/tiger/TIGER2016/' + settings.houses[house] + '/' + filename + '.zip';
 
     console.log('Downloading \'' + url + '\' ...');
 
     https.get(url, (res) => {
-        let filename = 'geo/' + state +'/' + house + '.zip';
+        let filename = 'geo/' + state +'/' + house + '.zip'
 
         fs.mkdirp('geo/' + state, (err) => {
             if (err) {
@@ -49,28 +35,85 @@ var doFetch = function () {
     });
 };
 
-var showHelp = function () {
+var showList = function () {
+    let dir = argv.m || settings.mapDir;
+    console.log("Listing map data in '" + dir + "/' ...");
+    
+    let list = [];
 
+    let states = fs.readdirSync(dir);
+    for (let s of states) {
+        let houses = fs.readdirSync(dir + '/' + s);
+
+        let hs = [];
+        for (let h of houses)
+            hs.push(h.split('.')[0])
+
+        list.push({
+            state: s.toUpperCase(),
+            houses: hs
+        });
+    }
+
+    console.log('');
+    for (let item of list) {
+        let l = item.houses.indexOf('lower') > -1,
+            u = item.houses.indexOf('upper') > -1;
+        let i = '';
+        if (l)
+            i += 'lower'
+        if (l & u) 
+            i += ' & '
+        if (u) 
+            i += 'upper'
+        
+        console.log('- ' + item.state + ': ' + i);
+    }
 };
 
-
-var cmd = process.argv[2];
-if (!cmd) {
-    console.error('Error: No command specified! Use \'help\' to get help.');
-    process.exit();
-} else {
-    cmd = cmd.toLowerCase();
-    switch (cmd) {
-        case 'help':
-            showHelp();
-            break;
-        case 'fetch':
-            doFetch();
-            break;        
-        default:
-            console.error('Error: Invalid command: ' + cmd);
-            process.exit(1);
-            break;
-    }
+let codes = '';
+let keys = Object.keys(settings.states).sort((a, b) => a < b ? -1 : (a > b ? 1 : 0));
+let c = 0;
+for (let key of keys) {
+    codes += key.toUpperCase();
+    c++;
+    if (c != keys.length)
+        codes += ', ';
 }
 
+var yargs = require('yargs')
+    .usage('Usage: $0 <state> <house> [options]\n\nParameters:\n  state:      ANSI code of state/territory (see below)\n  house:      Which house of the legislature (lower|upper)')
+    .help('h').alias('h', 'help')
+    .boolean('l').alias('l', 'list').describe('l', 'List currently downloaded maps')
+    .alias('m', 'mapDir').describe('m', 'Location of map data (overrides setting)').default('m', settings.mapDir)
+    .boolean('v').alias('v', 'verbose').describe('v', 'Show verbose output')
+    .epilog('Valid state/territory codes:\n' + codes) , argv = yargs.argv;    
+
+if (argv.l) {
+    showList();
+    process.exit();
+}
+
+if (argv._.length < 2) {
+    console.error('Error! Not enough arguments: got ' + argv._.length + ' but expected 2.\n')
+    yargs.showHelp();
+    process.exit();
+}
+
+let s = argv._[0].toLowerCase();
+let h = argv._[1].toLowerCase();
+
+if (!settings.states[s]) {
+    console.error('Error! Invalid state: \'' + argv._[0] + '\'. Use -l option to list valid codes');
+    process.exit();
+}
+
+if (!settings.houses[h]) {
+    console.error('Error! Invalid house: \'' + argv._[1] + '\'. Valid options are (lower|upper)');
+    process.exit();
+}
+
+console.log('Everything looks good: ')
+console.dir(argv._);
+
+doFetch(s, h);
